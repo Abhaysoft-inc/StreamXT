1873a5565a5c8c625c8155911fcbc0b777d54fe9
Abhaysoft-inc <av385735@gmail.com>
2025-06-08 00:29:34 +0530

    added the screen and the camera both on the canvas

--- src/components/dashboard/stream-to-youtube/Studio.jsx
+++ src/components/dashboard/stream-to-youtube/Studio.jsx
@@ -5,6 +5,7 @@ import React, { useEffect, useRef, useState } from 'react';
 const Studio = () => {
     const videoRef = useRef(null);
     const canvasRef = useRef(null);
+    const screenVideoRef = useRef(null);
     const animationFrameRef = useRef(null);
     const [isCameraEnabled, setisCameraEnabled] = useState(true);
     const [isAudioEnabled, setisAudioEnabled] = useState(true);
@@ -21,29 +22,48 @@ const Studio = () => {
             ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
 
             // Draw video frame
-            ctx.drawImage(videoRef.current, 0, 0, canvasRef.current.width, canvasRef.current.height);
+            ctx.drawImage(screenVideoRef.current, 0, 0, canvasRef.current.width, canvasRef.current.height);
+            ctx.drawImage(videoRef.current, canvasRef.current.width - 200, canvasRef.current.height - 90, 200, 90)
 
             // Request next frame
             animationFrameRef.current = requestAnimationFrame(drawCanvas);
+
+
         }
     };
 
     useEffect(() => {
         const startStream = async () => {
             try {
+
+                // Camera ki feed le rahe hai
                 const cameraStream = await navigator.mediaDevices.getUserMedia({
                     audio: isAudioEnabled,
                     video: isCameraEnabled,
                 });
 
-                if (videoRef.current) {
+                // Screen ki feed le rahe h
+
+                const screenStream = await navigator.mediaDevices.getDisplayMedia({
+                    audio: true,
+                    video: true
+                })
+
+                if (videoRef.current && screenVideoRef.current) {
                     videoRef.current.srcObject = cameraStream;
+                    screenVideoRef.current.srcObject = screenStream;
                     videoRef.current.onloadedmetadata = () => {
                         videoRef.current.play();
+                    };
+                    screenVideoRef.current.onloadedmetadata = () => {
+                        screenVideoRef.current.play();
                     };
                     videoRef.current.onplay = () => {
                         drawCanvas();
                     };
+                    screenVideoRef.current.onplay = () => {
+                        drawCanvas();
+                    }
                 }
             } catch (error) {
                 console.error('Stream error:', error);
@@ -87,25 +107,26 @@ const Studio = () => {
                         ref={canvasRef}
                         className="w-[630px] h-[355px] object-contain rounded"
                     ></canvas>
+                    <video ref={screenVideoRef} className='hidden'></video>
                 </div>
             </div>
 
             <div className="bottombar flex justify-center mt-30">
-                <div className="flex items-center gap-9 bg-gray-900 px-16 py-4 rounded-full text-white">
+                <div className="flex items-center gap-9 bg-[#171717] px-8 py-3 rounded-xl text-white">
                     <div className="cursor-pointer">
-                        <Mic size={29} onClick={toggleAudio} />
+                        <Mic size={25} onClick={toggleAudio} />
                     </div>
                     <div className="cursor-pointer">
-                        <Video size={29} onClick={toggleCamera} />
+                        <Video size={25} onClick={toggleCamera} />
                     </div>
                     <div className="cursor-pointer">
-                        <MonitorUp size={29} />
+                        <MonitorUp size={25} />
                     </div>
                     <div className="cursor-pointer">
-                        <UserPlus size={29} />
+                        <UserPlus size={25} />
                     </div>
                     <div className="cursor-pointer">
-                        <Button className={'cursor-pointer'}>Go Live</Button>
+                        <Button className={'cursor-pointer'} variant={"secondary"}>Go Live</Button>
                     </div>
                 </div>
             </div>
