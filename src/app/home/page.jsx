9224bfc0dabb948a1c7f248dae2f173f4c48432d
Abhaysoft-inc <av385735@gmail.com>
2025-06-07 09:15:54 +0530

    managed to stream the camera to  youtube

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,23 +1,56 @@
 "use client"
-import React, { useEffect, useRef } from 'react'
 
-const StreamingPage = () => {
+import React, { useEffect, useRef, useState } from 'react'
+import io from 'socket.io-client'
 
+const StreamPage = () => {
     const videoRef = useRef(null);
+    const ioRef = useRef(null);
+    const mediaRecorderRef = useRef(null);
 
     useEffect(() => {
+        ioRef.current = io('http://localhost:3001');
+
+        ioRef.current.on('welcome', (msg) => {
+            console.log(msg);
+        })
+
+
         const startStream = async () => {
             try {
+
                 const stream = await navigator.mediaDevices.getUserMedia({
                     audio: true,
-                    video: true
+                    video: true,
                 });
 
                 if (videoRef.current) {
                     videoRef.current.srcObject = stream;
                 }
 
+                // const mediaRecorder = new MediaRecorder(stream);
 
+                mediaRecorderRef.current = new MediaRecorder(stream, {
+                    imeType: 'video/webm;codecs=vp8,opus',
+                    videoBitsPerSecond: 2500000 // 2.5 Mbps
+                })
+
+
+                mediaRecorderRef.current.ondataavailable = (e) => {
+                    if (e.data && e.data.size > 0) {
+                        const reader = new FileReader();
+
+                        reader.onload = () => {
+                            const buffer = Buffer.from(reader.result)
+                            ioRef.current.emit('stream', buffer);
+                        };
+                        reader.readAsArrayBuffer(e.data);
+                    }
+                };
+
+                mediaRecorderRef.current.start(1000);
+
+                // ioRef.current.emit('stream', stream);
 
             } catch (error) {
                 console.log(error)
@@ -25,30 +58,29 @@ const StreamingPage = () => {
             }
         }
 
-        startStream();
+        // startStream();
 
         return () => {
-            if (videoRef.current?.srcObject) {
-                const tracks = videoRef.current.srcObject.getTracks();
-                tracks.forEach(track => track.stop());
+
+            if (mediaRecorderRef.current && mediaRecorderRef.current.state !== 'inactive') {
+                mediaRecorderRef.current.stop();
             }
+            if (ioRef.current) {
+                ioRef.current.disconnect();
+            }
+
         }
-    }, []
-
-    );
-
+    }, [])
 
 
     return (
         <>
 
-            <video id="user-video" ref={videoRef} autoPlay playsInline muted></video>
-
-            <button className='bg-white text-black'>Helo</button>
+            {/* <video ref={videoRef} autoPlay muted></video> */}
 
 
         </>
     )
 }
 
-export default StreamingPage
+export default StreamPage
