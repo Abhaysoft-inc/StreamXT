7870f8d6851a556a9a709d5998a1caad04933a4f
Abhaysoft-inc <av385735@gmail.com>
2025-06-27 22:29:10 +0530

    made the landing page responsive

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,24 @@
 "use client"
 import Navbar from "@/components/landing/Navbar";
-// import { DotPattern } from '@/components/magicui/dot-pattern';
+
 import { cn } from "@/lib/utils";
 import { TextAnimate } from "@/components/magicui/text-animate";
 import { Figtree } from 'next/font/google'
 import { Button } from "@/components/ui/button"
 import { IconArrowRight, IconBrandGithub } from "@tabler/icons-react"
-import { FaYoutube, FaTwitch, FaVimeoV, FaDailymotion } from "react-icons/fa";
+import { FaYoutube, FaTwitch, FaVimeoV, FaDailymotion, FaReddit, FaFacebook, FaFacebookF, FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { Safari } from "@/components/magicui/safari";
 import Notif, { notif } from '@/components/landing/notif'
 import dynamic from 'next/dynamic'
 import { Suspense } from 'react'
+
+import {
+  Marquee,
+  MarqueeItem,
+  MarqueeFade,
+  MarqueeContent,
+} from '@/components/ui/shadcn-io/marquee';
 
 const DotPattern = dynamic(
   () => import("@/components/magicui/dot-pattern").then((mod) => ({ default: mod.DotPattern })),
@@ -64,7 +71,7 @@ export default function Home() {
             <div className="flex justify-center pt-6">
 
 
-              <TextAnimate animation="slideUp" as="h1" by="word" delay={1} duration={1.5} className={`text-7xl text-center wrap px-62 whitespace-pre-line leading-none `}>
+              <TextAnimate animation="" as="h1" by="word" delay={1} duration={1.5} className={`text-5xl text-balance px-11 md:text-7xl text-center  md:px-62 whitespace-pre-line leading-none`}>
                 {"Lag-free streaming directly from your browser"}
               </TextAnimate>
             </div>
@@ -74,17 +81,55 @@ export default function Home() {
 
             </div>
 
-            <p className="text-center mt-10 flex justify-center gap-2 items-center">
-              Stream to multiple streaming platform with just one click <b className="flex gap-1 items-center cursor-pointer ">View all <IconArrowRight size={20} /> </b>
+            <p className=" px-3 text-center mt-10 flex justify-center gap-2 items-center">
+              Stream to multiple streaming platform with just one click <b className=" hidden md:flex gap-1 items-center cursor-pointer ">View all <IconArrowRight size={20} /> </b>
             </p>
 
-            <div className="  mt-10 flex justify-center gap-10">
-              <FaYoutube size={45} className="opacity-50 hover:opacity-90" />
-              <FaTwitch size={45} className="opacity-50 hover:opacity-90" />
-              <FaVimeoV size={45} className="opacity-50 hover:opacity-90" />
-              <FaDailymotion size={45} className="opacity-50 hover:opacity-90" />
-              <FaXTwitter size={45} className="opacity-50 hover:opacity-90" />
+            <div className="flex justify-center items-center gap-1 mt-2 cursor-pointer md:hidden">
+              <p className="">View all</p>
+              <IconArrowRight size={20} />
             </div>
+
+            <div className="mt-10 flex justify-center">
+              <div className="w-full max-w-4xl overflow-hidden">
+                <Marquee className="[--duration:20s]">
+                  <MarqueeFade side="left" />
+                  <MarqueeFade side="right" />
+                  <MarqueeContent pauseOnHover={false} className="flex items-center gap-24" >
+                    <MarqueeItem className="flex-shrink-0 mr-8">
+                      <FaYoutube size={60} className="opacity-20 hover:opacity-90" />
+                    </MarqueeItem>
+                    <MarqueeItem className="flex-shrink-0 mx-8">
+                      <FaTwitch size={60} className="opacity-20 hover:opacity-90" />
+                    </MarqueeItem>
+                    <MarqueeItem className="flex-shrink-0 mx-8">
+                      <FaVimeoV size={60} className="opacity-20 hover:opacity-90" />
+                    </MarqueeItem>
+                    <MarqueeItem className="flex-shrink-0 mx-8">
+                      <FaDailymotion size={60} className="opacity-20 hover:opacity-90" />
+                    </MarqueeItem>
+                    <MarqueeItem className="flex-shrink-0 mx-8">
+                      <FaXTwitter size={60} className="opacity-20 hover:opacity-90" />
+                    </MarqueeItem>
+                    <MarqueeItem className="flex-shrink-0 mx-8">
+                      <FaFacebookF size={60} className="opacity-20 hover:opacity-90" />
+                    </MarqueeItem>
+                    <MarqueeItem className="flex-shrink-0 ml-8">
+                      <FaInstagram size={60} className="opacity-20 hover:opacity-90" />
+                    </MarqueeItem>
+
+
+
+
+
+                  </MarqueeContent>
+                </Marquee>
+              </div>
+            </div>
+
+
+
+
 
 
 
@@ -119,19 +164,19 @@ export default function Home() {
 
         <div className="mt-25 mb-30">
 
-          <p className="text-center text-4xl px-30 font-bold">
+          <p className="text-center text-4xl px-6 text-balance md:px-30 font-bold">
             "Stream from <span className="text-orange-400">anywhere</span>, <span className="text-blue-400">anytime</span> with any device"
           </p>
 
 
-          <div className="mt-20 flex">
-            <div className="left w-1/2 mx-10">
+          <div className="mt-20 md:flex">
+            <div className="left md:w-1/2 mx-10">
               <Safari url="streamxt.live" className="size-full" imageSrc={"/screen.png"} />
 
 
             </div>
 
-            <div className="right w-1/2 px-10 font-semibold">
+            <div className="mt-8 md:mt-0 right md:w-1/2 px-10 font-semibold">
               <p className="text-2xl">Go live on multiple streaming platform in <br /> <span className="text-orange-500">one click!</span> </p>
 
               <p className="mt-6 font-xl font-normal">
